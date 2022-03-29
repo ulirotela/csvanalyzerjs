fe09ab56e59f88c7c901c1b2e8db4825c4abac41
Saqib Ali <business.saqib@gmail.com>
2022-03-29 22:18:21 +0500

    Added Filtering

--- src/render.js
+++ src/render.js
@@ -22,7 +22,32 @@ previousButton.addEventListener('click', (event)=> {
 nextButton.addEventListener('click', (event)=> {
     startingPoint += tableIncrement;
     renderTable();
-})
+});
+
+let constFilter = document.getElementById('const_name');
+let fNameFilter = document.getElementById('first_name');
+let lNameFilter = document.getElementById('last_name');
+
+var globalFilters = {
+    constName: '-1',
+    fName: '-1',
+    lName: '-1'
+}
+
+constFilter.addEventListener('change', (event)=> {
+    globalFilters.constName = event.target.value;
+    renderTable();
+});
+
+fNameFilter.addEventListener('change', (event)=> {
+    globalFilters.fName = event.target.value;
+    renderTable();
+});
+
+lNameFilter.addEventListener('change', (event) => {
+    globalFilters.lName = event.target.value;
+    renderTable();
+});
 
 ipc.on('selected-file', (event, file_details) => {
     
@@ -30,46 +55,106 @@ ipc.on('selected-file', (event, file_details) => {
     "Current File Path: " + `<p class="grey-text">${file_details.path}</p>`;
 
     global_file_details = file_details;
+    globalFilters = {
+        constName: '-1',
+        fName: '-1',
+        lName: '-1'
+    }
 
     let showButtons = document.querySelector('.btn-container');
+    let tableControls = document.querySelector('.table-controls');
+
     if (file_details.data.length > 0) {
         showButtons.classList.remove('hide');
+        tableControls.classList.remove('hide');
     }
     else {
         showButtons.classList.add('hide');
+        tableControls.classList.add('hide');
     }
 
+    renderFilterOptions();
     renderTable();
 
 
 });
 
-const renderTable = (file_details = null) => {
+const renderFilterOptions = (file_details = null)=> {
 
     if (!file_details) file_details = global_file_details;
 
+    firstNames = new Set(file_details.data.map(row => row['Candidate First Name'].toUpperCase()));
+    lastNames = new Set(file_details.data.map(row => row['Candidate surname'].toUpperCase()));
+    constNames = new Set(file_details.data.map(row => row['Constituency Name'].toUpperCase()));
+
+    fNameFilter.innerHTML = `<option value="-1">Select first name</option>`;
+    lNameFilter.innerHTML = `<option value="-1">Select last name</option>`;
+    constFilter.innerHTML = `<option value="-1">Select constituency</option>`;
+
+
+    // adding all unique values to the dropdown
+    firstNames.forEach(fname => {
+        fNameFilter.innerHTML += `<option value="${fname}">${fname}</option>`;
+    });
+
+    lastNames.forEach(lname => {
+        lNameFilter.innerHTML += `<option value="${lname}">${lname}</option>`;
+    });
+
+    constNames.forEach(constName => {
+        constFilter.innerHTML += `<option value="${constName}">${constName}</option>`;
+    });
+
+
+}
+
+const filterData = ({data}) => {
+
+    // filter by constituency
+    if (globalFilters.constName != '-1') {
+        data = data.filter(row => row['Constituency Name'].toUpperCase() == globalFilters.constName.toUpperCase());
+    }
+
+    // filter by first name
+    if (globalFilters.fName != '-1') {
+        data = data.filter(row => row['Candidate First Name'].toUpperCase() == globalFilters.fName.toUpperCase());
+    }
+
+    // filter by last name
+    if (globalFilters.lName != '-1') {
+        data = data.filter(row => row['Candidate surname'].toUpperCase() == globalFilters.lName.toUpperCase());
+    }
+
+    return data;
+
+}
+
+const renderTable = (file_details = null) => {
+
+    if (!file_details) file_details = global_file_details;
     if (startingPoint >= file_details.data.length) startingPoint = file_details.data.length - tableIncrement;
 
     let file_table_element = document.getElementById('file_data');
     file_table_element.innerHTML = "";
 
+    file_details = filterData(global_file_details);
+
     let file_table = '';
 
     file_table += `<thead><tr>`;
-    for (let i = 0; i < Object.keys(file_details.data[0]).length; i++) {
-        let keys = Object.keys(file_details.data[0]);
+    for (let i = 0; i < Object.keys(file_details[0]).length; i++) {
+        let keys = Object.keys(file_details[0]);
         let th = `<th>${keys[i]}</th>`;
         file_table += th;
     }
     file_table += `</tr></thead>`;
 
-    for (let i = startingPoint; i < startingPoint + tableIncrement && i < file_details.data.length; i++) {
-        file_table += getTableRow(file_details.data[i]);
+    for (let i = startingPoint; i < startingPoint + tableIncrement && i < file_details.length; i++) {
+        file_table += getTableRow(file_details[i]);
     }
 
     file_table_element.innerHTML = file_table;
 }
-
 
 const getTableRow = (row) => {
 
